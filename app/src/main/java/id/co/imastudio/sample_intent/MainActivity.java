9723cd8a68bd8ab437b317ad12ef05546b204b1c
ima2 <mursitaffandi@gmail.com>
2016-10-19 00:03:53 +0700

    change intent to sms

--- app/src/main/java/id/co/imastudio/sample_intent/MainActivity.java
+++ app/src/main/java/id/co/imastudio/sample_intent/MainActivity.java
@@ -4,26 +4,29 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     private Button btnSub1, btnSub2, btnDial;
     private String strIntent;
     private EditText txtIntent;
+    SmsManager smsManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnSub1 = (Button)findViewById(R.id.btn_activity_sub_1);
-        btnSub2 = (Button)findViewById(R.id.btn_activity_sub_2);
-        btnDial = (Button)findViewById(R.id.btn_activity_dial);
+        btnSub1 = (Button) findViewById(R.id.btn_activity_sub_1);
+        btnSub2 = (Button) findViewById(R.id.btn_activity_sub_2);
+        btnDial = (Button) findViewById(R.id.btn_activity_dial);
 
-        txtIntent = (EditText)findViewById(R.id.text_Intent);
+        txtIntent = (EditText) findViewById(R.id.text_Intent);
 
+        smsManager = SmsManager.getDefault();
         btnSub1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,13 +38,8 @@ public class MainActivity extends AppCompatActivity
         btnSub2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                strIntent = "";
-                strIntent = txtIntent.getText().toString();
-                txtIntent.setError(null);
-                if (strIntent.matches("")){
-                    txtIntent.setError("This field is required");
-                    txtIntent.requestFocus();
-                } else {
+                if (getEditextContent()) {
+                    txtIntent.setError(null);
                     Intent intent = new Intent(MainActivity.this, Sub2Activity.class);
                     intent.putExtra(Sub2Activity.KEY_DATA, strIntent);
                     startActivityForResult(intent, 0);
@@ -49,13 +47,31 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+
         btnDial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:082121212093"));
-                startActivity(intent);
+                if (getEditextContent()) {
+                    Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                    sendIntent.putExtra("address"  ,"+6282243609178");
+                    sendIntent.putExtra("sms_body", strIntent);
+                    sendIntent.setType("vnd.android-dir/mms-sms");
+                    startActivity(sendIntent);
+                }
             }
         });
+    }
+
+    private boolean getEditextContent() {
+        this.strIntent = "";
+        this.strIntent = this.txtIntent.getText().toString();
+        if (this.strIntent.matches("")) {
+            this.txtIntent.setError("This field is required");
+            this.txtIntent.requestFocus();
+            return false;
+        } else {
+            return true;
+        }
     }
 }
 
